c9c7f2ebf85981150ea66782b9e59b17ffe470d0
Tom Gallacher <tomgallacher23@gmail.com>
2013-02-15 11:09:54 +0000

    Adding first pass at custom example

---
+++ example/js/example-custom.js
@@ -0,0 +1,45 @@
+(function () {
+
+  var names =
+  [ 'Taylor Rouillard'
+  , 'Aline Courter'
+  , 'Rene Viens'
+  , 'Dana Marinez'
+  , 'Simone Belden'
+  , 'Merrilee Mazur'
+  , 'Phil Yearta'
+  , 'Inga Whitaker'
+  , 'Larhonda Savard'
+  , 'Wilbur Audia'
+  , 'Constance Maier'
+  , 'Lyle Deschenes'
+  , 'Frankie Bad'
+  , 'Sharie Veneziano'
+  , 'Marlyn Ciccone'
+  , 'Arvilla Kirshner'
+  , 'Sade Simas'
+  , 'Larraine Cottle'
+  , 'Dona Lipton'
+  , 'Hilario Goodenough'
+  ]
+
+  var template = $('#result-template').html()
+
+  var p = new window.Preempt($('input[name=search]'),
+    { query: function (term, callback) {
+        callback(
+          names
+            .filter(function (name) {
+              return name.toUpperCase().indexOf(term.toUpperCase()) !== -1
+            })
+            .map(function (name) {
+              return { href: '#/' + name, text: name }
+            })
+        )
+      }
+    , $root: $('.container.preempt')
+    , template: _.template($('#result-template').html())
+  })
+  p.init()
+
+}())
